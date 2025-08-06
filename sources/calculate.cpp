b5c9f3caceffdc6f135e0bf2767ed95947584c9c
anton <anton@debian.net>
2025-08-06 22:58:58 +0500

    feat(score): add strike calculation logic

--- sources/calculate.cpp
+++ sources/calculate.cpp
@@ -9,9 +9,19 @@ unsigned short calculate_score(vector<vector<unsigned short>>& game, int i)
 
     if(i>0)
     {
-	if(game[i-1].size()==2 && game[i-1][game[i-1].size()-1]==10) //спэа
-	    game[i-1][game[i-1].size()-1]+=game[i][0]; //"Spare считается как 10 кеглей плюс количество кеглей, сбитых в следующем броске"
-        result=game[i-1][game[i-1].size()-1];
+	if(game[i-1][game[i-1].size()-1]==10)
+	    if(game[i-1].size()==2) //спэа
+		game[i-1][game[i-1].size()-1]+=game[i][0]; //"Spare считается как 10 кеглей плюс количество кеглей, сбитых в следующем броске"
+    	    else //страйк
+	    {
+		if(i>1) //двойные, тройные
+		{
+			if(game[i-2].size()==1 && (game[i-2][game[i-2].size()-1]%10==0)) //если два фрейма назад был страйк
+			    game[i].size()==1?game[i-2][game[i-2].size()-1]+=game[i][game[i].size()-1]:game[i-2][game[i-2].size()-1]+=game[i][0];
+		}
+		game[i-1][game[i-1].size()-1]+=game[i][game[i].size()-1]; //"Для strike вы добавляете значение следующих двух бросков к 10 кеглям"
+	    }
+	result=game[i-1][game[i-1].size()-1];
     }
     else
         result=game[i][game[i].size()-1];
