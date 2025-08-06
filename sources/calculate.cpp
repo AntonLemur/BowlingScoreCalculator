047fe74d926d2e89d4aac754cbb3972368e52195
anton <anton@debian.net>
2025-08-06 17:54:00 +0500

    feat(spare): add spare calculation logic

--- sources/calculate.cpp
+++ sources/calculate.cpp
@@ -11,3 +11,18 @@ unsigned short calculate_score(vector<unsigned short>& frame)
 
     return result;
 }
+
+unsigned short calculate_score_spare(vector<vector<unsigned short>>& game, int i)
+{
+    unsigned short result=0;
+
+    if(i>0)
+    {
+	if(game[i-1].size()==2 && game[i-1][game[i-1].size()-1]==10) //спэа
+	    game[i-1][game[i-1].size()-1]+=game[i][0]; //"Spare считается как 10 кеглей плюс количество кеглей, сбитых в следующем броске"
+    }
+
+    result=game[i-1][game[i-1].size()-1];
+    return result;
+}
+
