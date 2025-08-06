2fe112bcfaf651d1e7cd0f91aa04690e2bec7a24
anton <anton@debian.net>
2025-08-07 00:10:21 +0500

    fix(score): strike calculation logic

--- sources/calculate.cpp
+++ sources/calculate.cpp
@@ -16,8 +16,8 @@ unsigned short calculate_score(vector<vector<unsigned short>>& game, int i)
 	    {
 		if(i>1) //двойные, тройные
 		{
-			if(game[i-2].size()==1 && (game[i-2][game[i-2].size()-1]%10==0)) //если два фрейма назад был страйк
-			    game[i].size()==1?game[i-2][game[i-2].size()-1]+=game[i][game[i].size()-1]:game[i-2][game[i-2].size()-1]+=game[i][0];
+			if(game[i-2].size()==1 && game[i-1].size()==1 && (game[i-2][game[i-2].size()-1]%10==0)) //если два фрейма назад был страйк
+			    game[i-2][0]+=game[i][0];
 		}
 		game[i-1][game[i-1].size()-1]+=game[i][game[i].size()-1]; //"Для strike вы добавляете значение следующих двух бросков к 10 кеглям"
 	    }
