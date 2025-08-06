c0c2ee3b5934386c0ba5fb837035ccd7aad9da03
anton <anton@debian.net>
2025-08-06 11:24:28 +0500

    Первый тест прошёл

--- sources/calculate.cpp
+++ sources/calculate.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include "calculate.h"
 
 using namespace std;
@@ -7,6 +6,8 @@ using namespace std;
 unsigned short calculate_score(vector<unsigned short>& frame)
 {
     unsigned short result=0;
+
+    result=frame[frame.size()-1];
 
     return result;
 }
