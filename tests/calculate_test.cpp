37914dfbe3415e1c1345762ae2d7ff9214efa978
anton <anton@debian.net>
2025-08-06 19:28:31 +0500

    refactor: made a calculation of the score in one function

--- tests/calculate_test.cpp
+++ tests/calculate_test.cpp
@@ -11,6 +11,7 @@ using namespace std;
 TEST(BowlingScoreCalculator, CalcScoreOneOpenFrame)
 {
     unsigned short frame_result=0;
+    vector<vector<unsigned short>> game;
     vector<unsigned short> frame;
     
     for(int j=0;j<2;j++) //броски
@@ -28,7 +29,8 @@ TEST(BowlingScoreCalculator, CalcScoreOneOpenFrame)
     for(int j=0;j<frame.size();j++)
 	cout<<"Бросок "<<j+1<<", результат фрейма - "<<frame[j]<<endl;
     
-    ASSERT_EQ(calculate_score(frame), frame_result);
+    game.push_back(frame);
+    ASSERT_EQ(calculate_score(game,0), frame_result);
 }
 
 TEST(BowlingScoreCalculator, CalcScoreSpare)
@@ -62,10 +64,39 @@ TEST(BowlingScoreCalculator, CalcScoreSpare)
 	game.push_back(frame);
 	if(i>0)
 	{
-    	    unsigned short res=calculate_score_spare(game,i);
+    	    unsigned short res=calculate_score(game,i);
 	    //цена фрейма фактически не записывается до тех пор, пока вы не перейдете к следующему фрейму
     	    ASSERT_EQ(game[i-1][game[i-1].size()-1], prev_frame_result+frame[0]); //проверка результата предыдущего фрейма
 	}
 	prev_frame_result=frame[frame.size()-1];
     }
+
+
+    //отображение игры
+    for(int i=0;i<game.size();i++)
+    cout<<setw(10)<<i+1<<"|";
+    cout<<endl;
+    for(int i=0;i<game.size();i++)
+    cout<<"__________"<<"|";
+    cout<<endl;
+    for(int i=0;i<game.size();i++)
+    {
+    cout<<setw(10)<<left<<game[i][0]<<"|";
+    }
+    cout<<endl;
+    for(int i=0;i<game.size();i++)
+    {
+    stringstream ss;
+        ss<<game[i][game[i].size()-1];
+
+        int totalPadding = 10 - ss.str().length();
+    int leftPadding = totalPadding / 2;
+    int rightPadding = totalPadding - leftPadding;
+
+    cout << setw(leftPadding + ss.str().length()) << right << ss.str()
+              << setw(rightPadding) << "" <<"|";
+    }
+    cout<<endl;
+    cout<<endl;
+
 }
