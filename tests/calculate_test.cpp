847abc46d73160feabe5f34e6a8fb406c24c5f9c
anton <anton@debian.net>
2025-08-08 12:46:29 +0500

    fix(score)!: changed frame structure in test

--- tests/calculate_test.cpp
+++ tests/calculate_test.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <numeric> // Required for std::accumulate
+#include <algorithm>
 #include "calculate.h"
 
 using namespace std;
@@ -19,9 +21,9 @@ TEST(BowlingScoreCalculator, CalcScoreOneOpenFrame)
 	unsigned short pins;
 	cout<<"Введите количество сбитых кеглей:"<<endl;
 	cin>>pins;
-	frame_result+=pins;
-	frame.push_back(frame_result);
-	if(frame_result==10)
+        frame.push_back(pins);
+        frame_result+=pins;
+        if(pins==10)
 	    break;
     }
 
@@ -44,32 +46,47 @@ TEST(BowlingScoreCalculator, CalcScoreSpare)
     for(int i=0;i<cnt_frames;i++) //фреймы
     {
         vector<unsigned short> frame;
-        unsigned short frame_result=0;
 	for(int j=0;j<2;j++) //броски
 	{
 	    unsigned short pins;
-	    cout<<"Введите количество сбитых кеглей:"<<endl;
+	    cout<<"Фрейм "<<i+1<<", бросок "<<j+1<<endl<<"Введите количество сбитых кеглей:"<<endl;
 	    cin>>pins;
-	    frame_result+=pins;
-	    frame.push_back(frame_result);
-	    if(frame_result==10)
+	    if(pins>10)
+	    {
+		cout<< "\033[" << 31 << "m" <<"Значение не должно быть больше 10."<< "\033[" << 39 << "m"<<endl;
+		j--;
+		continue;
+	    }
+	    frame.push_back(pins);
+	    if(pins==10)
 		break;
 	}
-
-        cout<<"Фрейм - "<<i+1<<endl;
-        cout<<"Бросков - "<<frame.size()<<endl;
-        for(int j=0;j<frame.size();j++)
-	    cout<<"Бросок "<<j+1<<", результат фрейма - "<<frame[j]<<endl;
+        unsigned short current_sum=accumulate(frame.begin(), frame.end(),0);
 
 	game.push_back(frame);
 	if(i>0)
 	{
-    	    unsigned short res=calculate_score(game,i);
-	    //цена фрейма фактически не записывается до тех пор, пока вы не перейдете к следующему фрейму
-    	    ASSERT_EQ(game[i-1][game[i-1].size()-1], prev_frame_result+frame[0]); //проверка результата предыдущего фрейма
+	    prev_frame_result+=frame[0];
+    	    ASSERT_EQ(calculate_score(game,i), prev_frame_result); //проверка результата предыдущего фрейма
 	}
-	prev_frame_result=frame[frame.size()-1];
+	else
+    	    ASSERT_EQ(calculate_score(game,i), current_sum); //проверка результата предыдущего фрейма
+
+	prev_frame_result=current_sum;
     }
+
+    //печать сохранённой игры
+    for_each(game.begin(), game.end(),
+        [](const vector<unsigned short>& frame) {
+            stringstream ss;
+            for_each(frame.begin(), frame.end(), 
+                [&ss](unsigned short item) {
+                    ss << item << ",";
+                });
+	    string s=ss.str();
+	    s.pop_back();
+            cout<<"["<<s<<"]"<<endl;
+        });
 }
 
 TEST(BowlingScoreCalculator, CalcScoreStrike)
@@ -83,57 +100,46 @@ TEST(BowlingScoreCalculator, CalcScoreStrike)
     for(int i=0;i<cnt_frames;i++) //фреймы
     {
         vector<unsigned short> frame;
-        unsigned short frame_result=0;
 	for(int j=0;j<2;j++) //броски
 	{
 	    unsigned short pins;
-	    cout<<"Введите количество сбитых кеглей:"<<endl;
+	    cout<<"Фрейм "<<i+1<<", бросок "<<j+1<<endl<<"Введите количество сбитых кеглей:"<<endl;
 	    cin>>pins;
-	    frame_result+=pins;
-	    frame.push_back(frame_result);
-	    if(frame_result==10)
+	    if(pins>10)
+	    {
+		cout<< "\033[" << 31 << "m" <<"Значение не должно быть больше 10."<< "\033[" << 39 << "m"<<endl;
+		j--;
+		continue;
+	    }
+	    frame.push_back(pins);
+	    if(pins==10)
 		break;
 	}
-
-        cout<<"Фрейм - "<<i+1<<endl;
-        cout<<"Бросков - "<<frame.size()<<endl;
-        for(int j=0;j<frame.size();j++)
-	    cout<<"Бросок "<<j+1<<", результат фрейма - "<<frame[j]<<endl;
+        unsigned short current_sum=accumulate(frame.begin(), frame.end(),0);
 
 	game.push_back(frame);
 	if(i>0)
 	{
-    	    unsigned short res=calculate_score(game,i);
-	    //цена фрейма фактически не записывается до тех пор, пока вы не перейдете к следующему фрейму
-    	    ASSERT_EQ(game[i-1][game[i-1].size()-1], prev_frame_result+frame[frame.size()-1]); //проверка результата предыдущего фрейма
+    	    unsigned short current_frame_result=accumulate(frame.begin(), frame.end(), 0);
+	    prev_frame_result+=current_frame_result;
+    	    ASSERT_EQ(calculate_score(game,i), prev_frame_result); //проверка результата предыдущего фрейма
 	}
-	prev_frame_result=frame[frame.size()-1];
+	else
+    	    ASSERT_EQ(calculate_score(game,i), current_sum); //проверка результата предыдущего фрейма
+
+	prev_frame_result=current_sum;
     }
 
-    //отображение игры
-    for(int i=0;i<game.size();i++)
-    cout<<setw(10)<<i+1<<"|";
-    cout<<endl;
-    for(int i=0;i<game.size();i++)
-    cout<<"__________"<<"|";
-    cout<<endl;
-    for(int i=0;i<game.size();i++)
-    {
-    cout<<setw(10)<<left<<game[i][0]<<"|";
-    }
-    cout<<endl;
-    for(int i=0;i<game.size();i++)
-    {
-    stringstream ss;
-        ss<<game[i][game[i].size()-1];
-
-        int totalPadding = 10 - ss.str().length();
-    int leftPadding = totalPadding / 2;
-    int rightPadding = totalPadding - leftPadding;
-
-    cout << setw(leftPadding + ss.str().length()) << right << ss.str()
-              << setw(rightPadding) << "" <<"|";
-    }
-    cout<<endl;
-    cout<<endl;
+    //печать сохранённой игры
+    for_each(game.begin(), game.end(),
+        [](const vector<unsigned short>& frame) {
+            stringstream ss;
+            for_each(frame.begin(), frame.end(), 
+                [&ss](unsigned short item) {
+                    ss << item << ",";
+                });
+	    string s=ss.str();
+	    s.pop_back();
+            cout<<"["<<s<<"]"<<endl;
+        });
 }
