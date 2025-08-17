8b4f605ae9bd85be72503555ba11fe276ff46dfc
anton <anton@debian.net>
2025-08-17 13:27:40 +0500

    test(score): add unit test for strike scoring

--- tests/calculate_test.cpp
+++ tests/calculate_test.cpp
@@ -46,3 +46,29 @@ TEST(BowlingScoreCalculator, CalcScoreSpare)
     cout<<"Тест EXPECT_EQ(game[1][game[1].size()-1], 1+9+1+1+2)"<<endl;
     EXPECT_EQ(game[1][game[1].size()-1], 1+9+1+1+2); //проверка результата текущего фрейма
 }
+
+TEST(BowlingScoreCalculator, CalcScoreStrike)
+{
+    vector<vector<unsigned short>> game_const={{10},{1,2}}; //игра
+    vector<vector<unsigned short>> game=game_const;
+
+    cout<<"game: "<<endl;
+    for_each(game.begin(), game.end(),
+        [](const vector<unsigned short>& frame) {
+            stringstream ss;
+            for_each(frame.begin(), frame.end(), 
+                [&ss](unsigned short item) {
+                    ss << item << ",";
+                });
+        string s=ss.str();
+        s.pop_back();
+            cout<<"["<<s<<"]"<<endl;
+    });
+
+    unsigned short res=calculate_score(game,1);
+    cout<<"Тест EXPECT_EQ(game[0][game[0].size()-1], 1+9+1)"<<endl;
+    //цена фрейма фактически не записывается до тех пор, пока вы не перейдете к следующему фрейму
+    EXPECT_EQ(game[0][game[0].size()-1], 10+1+2); //проверка результата предыдущего фрейма
+    cout<<"Тест EXPECT_EQ(game[1][game[1].size()-1], 1+9+1+1+2)"<<endl;
+    EXPECT_EQ(game[1][game[1].size()-1], 10+1+2+1+2); //проверка результата текущего фрейма
+}
