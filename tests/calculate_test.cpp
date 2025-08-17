ac757133873a9826394735394b0e9f0d8d9cf03f
anton <anton@debian.net>
2025-08-17 16:14:13 +0500

    test(score): add unit test for double strike scoring

--- tests/calculate_test.cpp
+++ tests/calculate_test.cpp
@@ -23,8 +23,8 @@ TEST(BowlingScoreCalculator, CalcScoreOneOpenFrame)
 
 TEST(BowlingScoreCalculator, CalcScoreSpare)
 {
-    vector<vector<unsigned short>> game_const={{1,9},{1,2}}; //игра
-    vector<vector<unsigned short>> game=game_const;
+    vector<vector<unsigned short>> game_const={{1,9},{1,2}}; //броски
+    vector<vector<unsigned short>> game=game_const; //игра
 
     cout<<"game: "<<endl;
     for_each(game.begin(), game.end(),
@@ -49,8 +49,8 @@ TEST(BowlingScoreCalculator, CalcScoreSpare)
 
 TEST(BowlingScoreCalculator, CalcScoreStrike)
 {
-    vector<vector<unsigned short>> game_const={{10},{1,2}}; //игра
-    vector<vector<unsigned short>> game=game_const;
+    vector<vector<unsigned short>> game_const={{10},{1,2}}; //броски
+    vector<vector<unsigned short>> game=game_const; //игра
 
     cout<<"game: "<<endl;
     for_each(game.begin(), game.end(),
@@ -71,4 +71,33 @@ TEST(BowlingScoreCalculator, CalcScoreStrike)
     EXPECT_EQ(game[0][game[0].size()-1], 10+1+2); //проверка результата предыдущего фрейма
     cout<<"Тест EXPECT_EQ(game[1][game[1].size()-1], 10+1+2+1+2)"<<endl;
     EXPECT_EQ(game[1][game[1].size()-1], 10+1+2+1+2); //проверка результата текущего фрейма
+}
+
+TEST(BowlingScoreCalculator, CalcScoreDoubleStrike)
+{
+    vector<vector<unsigned short>> game_const={{10},{10},{1,2}}; //броски 
+    vector<vector<unsigned short>> game=game_const;//игра
+
+    cout<<"game: "<<endl;
+    for_each(game.begin(), game.end(),
+        [](const vector<unsigned short>& frame) {
+            stringstream ss;
+            for_each(frame.begin(), frame.end(), 
+                [&ss](unsigned short item) {
+                    ss << item << ",";
+                });
+        string s=ss.str();
+        s.pop_back();
+            cout<<"["<<s<<"]"<<endl;
+    });
+
+    unsigned short res=calculate_score(game,1);
+    res=calculate_score(game,2);
+    cout<<"Тест EXPECT_EQ(game[0][game[0].size()-1], 10+10+1)"<<endl;
+    //цена фрейма фактически не записывается до тех пор, пока вы не перейдете к следующему фрейму
+    EXPECT_EQ(game[0][game[0].size()-1], 10+10+1); //проверка результата предпредпоследнего фрейма
+    cout<<"Тест EXPECT_EQ(game[1][game[1].size()-1], 10+10+1+10+1+2)"<<endl;
+    EXPECT_EQ(game[1][game[1].size()-1], 10+10+1+10+1+2); //проверка результата предыдущего фрейма
+    cout<<"Тест EXPECT_EQ(game[2][game[2].size()-1], 10+10+1+10+1+2+1+2)"<<endl;
+    EXPECT_EQ(game[2][game[2].size()-1], 10+10+1+10+1+2+1+2); //проверка результата текущего фрейма
 }
