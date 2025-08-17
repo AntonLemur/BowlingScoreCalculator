8dd9fc6af3c559b4c97a76e2b940246b29c27d23
anton <anton@debian.net>
2025-08-17 17:44:24 +0500

    test(score): add unit test for triple strike scoring

--- tests/calculate_test.cpp
+++ tests/calculate_test.cpp
@@ -106,3 +106,33 @@ TEST(BowlingScoreCalculator, CalcScoreDoubleStrike)
     cout<<"Тест EXPECT_EQ(game[2][game[2].size()-1], 10+10+1+10+1+2+1+2)"<<endl;
     EXPECT_EQ(game[2][game[2].size()-1], 10+10+1+10+1+2+1+2); //проверка результата текущего фрейма
 }
+
+TEST(BowlingScoreCalculator, CalcScoreTripleStrike)
+{
+    vector<vector<unsigned short>> game_const={{10},{10},{10}}; //броски 
+    vector<vector<unsigned short>> game=game_const;//игра
+
+    cout<<"game: "<<endl;
+    for_each(game.begin(), game.end(),
+        [](const vector<unsigned short>& frame) {
+            stringstream ss;
+            for_each(frame.begin(), frame.end(), 
+                [&ss](unsigned short item) {
+                    ss << item << ",";
+                });
+        string s=ss.str();
+        s.pop_back();
+            cout<<"["<<s<<"]"<<endl;
+    });
+
+    unsigned short res=calculate_score(game,0);
+    res=calculate_score(game,1);
+    res=calculate_score(game,2);
+    cout<<"Тест EXPECT_EQ(game[0][game[0].size()-1], 10+10+10)"<<endl;
+    //цена фрейма фактически не записывается до тех пор, пока вы не перейдете к следующему фрейму
+    EXPECT_EQ(game[0][game[0].size()-1], 10+10+10); //проверка результата предпредпоследнего фрейма
+    cout<<"Тест EXPECT_EQ(game[1][game[1].size()-1], 10+10+10+10+10)"<<endl;
+    EXPECT_EQ(game[1][game[1].size()-1], 10+10+10+10+10); //проверка результата предыдущего фрейма
+    cout<<"Тест EXPECT_EQ(game[2][game[2].size()-1], 10+10+10+10+10+10)"<<endl;
+    EXPECT_EQ(game[2][game[2].size()-1], 10+10+10+10+10+10); //проверка результата текущего фрейма
+}
